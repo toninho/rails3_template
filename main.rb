0b1401946dbddee6697f7bdee0b50d61e6e6c6d2
Fernando Meyer <fmcamargo@gmail.com>
2010-06-11 03:07:32 -0300

    tirando blueprint, adicionando mongoid

--- main.rb
+++ main.rb
@@ -1,49 +1,49 @@
 run "rm -Rf README public/index.html public/javascripts/* test app/views/layouts/*"
 
+gem 'rails', '3.0.0.beta3'
+
+gem "mongo_ext" 
+gem "mongoid", "2.0.0.beta6"
+gem "bson_ext", "1.0.1"
+
 gem 'haml', '>=3.0.4'
-gem 'inherited_resources', '>=1.1.2'
-gem 'will_paginate', '>=3.0.pre'
-gem 'devise', '>=1.1.rc1'
-gem 'formtastic', '>=0.9.8'
-gem "compass", ">= 0.10.1"
-
-gem 'rspec', '>=2.0.0.beta.8', :group => :test
-gem 'rspec-rails', '>=2.0.0.beta.8', :group => :test
-gem "factory_girl", :git => "git://github.com/thoughtbot/factory_girl.git", :branch => 'rails3', :group => :test
-
-gem 'cucumber', ">=0.6.3", :group => :cucumber
-gem 'cucumber-rails', ">=0.3.0", :group => :cucumber
-gem 'capybara', ">=0.3.6", :group => :cucumber
-gem 'database_cleaner', ">=0.5.0", :group => :cucumber
-
-gem 'inploy'
 
 gem 'rails3-generators', :git => "git://github.com/indirect/rails3-generators.git"
 
+gem "rcov",  :group => :test
+gem 'rspec', '>=2.0.0.beta.8', :group => :test
+gem 'rspec-rails', '>=2.0.0.beta.8', :group => :test
+gem "machinist",        :git => "git://github.com/notahat/machinist.git",  :group => :test
+gem "faker",  :group => :test
+gem "ZenTest",  :group => :test
+gem "autotest",  :group => :test
+gem "autotest-rails",  :group => :test
+gem "cucumber",         :git => "git://github.com/aslakhellesoy/cucumber.git",  :group => :test
+gem "database_cleaner", :git => 'git://github.com/bmabey/database_cleaner.git',  :group => :test
+gem "cucumber-rails",   :git => "git://github.com/aslakhellesoy/cucumber-rails.git",  :group => :test
+gem "capybara",  :group => :test
+gem "capybara-envjs",  :group => :test
+gem "launchy",  :group => :test
+gem "ruby-debug",  :group => :test
+
 application  <<-GENERATORS 
 config.generators do |g|
+  g.orm :mongoid
   g.template_engine :haml
-  g.test_framework  :rspec, :fixture => true, :views => false
+  g.test_framework  :rspec, :fixture => false, :views => false
   g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
 end
 GENERATORS
 
 run "bundle install"
 generate "rspec:install"
-generate "cucumber:skeleton --capybara --rspec --spork"
-run "gem install compass"
-run "compass init --using blueprint --app rails"
+generate "cucumber:install --capybara --rspec "
+generate "mongoid:config"
 
 get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
-get "http://github.com/dcrec1/rails3_template/raw/master/gitignore" ,".gitignore" 
-get "http://github.com/dcrec1/rails3_template/raw/master/screen.scss", "app/stylesheets/screen.scss"
-get "http://github.com/dcrec1/rails3_template/raw/master/application.html.haml", "app/views/layouts/application.html.haml"
-
-create_file 'config/deploy.rb', <<-DEPLOY
-application = '#{app_name}'
-repository = ''
-hosts = %w() 
-DEPLOY
+get "http://code.jquery.com/jquery-1.4.2.min.js", "public/javascripts/jquery/jquery-1.4.2.min.js"
+get "http://github.com/fmeyer/rails3_template/raw/master/gitignore" ,".gitignore" 
+get "http://github.com/fmeyer/rails3_template/raw/master/application.html.haml", "app/views/layouts/application.html.haml"
 
 git :init
 git :add => '.'
